a7ba870bd58ac7ddeac42f21a9350069d732c399
xuansining <1357821160@qq.com>
2020-09-25 22:24:46 +0800

    新建三个页面â

---
+++ src/router/index.ts
@@ -0,0 +1,46 @@
+import Vue from 'vue';
+import VueRouter, {RouteConfig} from 'vue-router';
+import Home from '../views/Home.vue';
+import Money from '@/views/Money.vue';
+import Label from '@/views/Label.vue';
+import Statistic from '@/views/Statistic.vue';
+
+Vue.use(VueRouter);
+
+const routes: Array<RouteConfig> = [
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
+  {
+    path:'/',
+    redirect:'/Money'
+  },
+  {
+    path:'/Money',
+    component:Money
+
+  }
+  ,{
+    path: '/Label',
+    component: Label
+  },{
+    path:'/Statistic',
+    component: Statistic
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
---
+++ src/views/Money.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>记账</div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Money'
+  };
+</script>
+
+<style scoped lang="scss">
+
+</style>
